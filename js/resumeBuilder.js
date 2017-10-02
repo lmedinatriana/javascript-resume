ca2971daa51439a76259785dbde7746974352b09
Luis Medina triana <Luis@Luiss-MacBook-Pro.local>
2017-10-01 23:53:29 -0400

    for loop changes

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,24 +1,50 @@
 var bio = {
     "name": "Luis Medina Triana",
     "role": "Web Designer",
-    "welcomeMessage": "I am a versatile multimedia strategist with broad skill set. Web Development, Digital Marketing and Graphic Design.",
-    "image": "images/profile.jpeg",
     "contacts": [{
         "mobile": "347-499-6308",
         "github": "lmedinatriana",
         "location": "New York City",
         "email": "miguemetri@gmail.com"
     }],
-    "skills": ["HTML5", "CSS3", "Media Query", "JavaScript", " P5", "Bootstrap", "jQuery", "CMS", "Ektron", "GitHub"]
+    "welcomeMessage": "I am a versatile multimedia strategist with broad skill set. Web Development, Digital Marketing and Graphic Design.",
+    "skills": ["HTML5", "CSS3", "Media Query", "JavaScript", " P5", "Bootstrap", "jQuery", "CMS", "Ektron", "GitHub"],
+    "biopic": "images/profile.jpeg"
+};
+
+var education = {
+    "schools": [{
+            "name": "Baruch College, Zicklin School of Business",
+            "location": "New York City",
+            "degree": "BBA",
+            "majors": ["Digital Marketing & Graphic Design"],
+            "dates": "2013 - 2016",
+            "url": "http://www.baruch.cuny.edu/"
+        },
+        {
+            "name": "LaGuardia Community College",
+            "location": "Long Island City",
+            "degree": "AS",
+            "majors": ["Business Administration"],
+            "dates": "2010 - 2013",
+            "url": "http://www.laguardia.edu/"
+        }
+    ],
+    "online courses": [{
+        "title": "Front-End Web Developer Nanodegree",
+        "school": "Udacity",
+        "dates": "2017 - Present",
+        "url": "https://www.udacity.com/course/nd001"
+    }]
 };
 
 var work = {
     "jobs": [{
         "employer": "LaGuardia Community College  ",
         "title": "Web Designer",
-        "dates": "2013 - Present",
         "location": "Long Island City",
-        "description": "signing and developing responsive web-pages using HTML5, CSS3, JavaScript, Bootstrap and Ektron widgets. The site averages about 1.5 Million users and 26 Million page views annually."
+        "dates": "2013 - Present",
+        "description": "Designing and developing responsive web-pages using HTML5, CSS3, JavaScript, Bootstrap and Ektron widgets. The site averages about 1.5 Million users and 26 Million page views annually."
     }]
 };
 
@@ -38,45 +64,21 @@ var projects = {
     ]
 };
 
-var education = {
-    "schools": [{
-            "name": "Baruch College, Zicklin School of Business",
-            "location": "New York City",
-            "degree": "BBA",
-            "majors": "Digital Marketing & Graphic Design",
-            "dates": "2013 - 2016",
-            "url": "http://www.baruch.cuny.edu/"
-        },
-        {
-            "name": "LaGuardia Community College",
-            "location": "Long Island City",
-            "degree": "AS",
-            "majors": "Business Administration",
-            "dates": "2010 - 2013",
-            "url": "http://www.laguardia.edu/"
-        }
-    ],
-    "online courses": [{
-        "title": "Front-End Web Developer Nanodegree",
-        "school": "Udacity",
-        "dates": "2017 - Present",
-        "url": "https://www.udacity.com/course/nd001"
-    }]
-};
+
 
 
 bio.display = function() {
     var formattedName = HTMLheaderName.replace("%data%", bio.name);
     var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-    var formattedImage = HTMLbioPic.replace("%data%", bio.image);
+    var formattedBioPic = HTMLbioPic.replace("%data%", bio.biopic);
     var formattedMessage = HTMLWelcomeMsg.replace("%data%", bio.welcomeMessage);
 
-    $("#header").prepend(formattedRole).prepend(formattedName).append(formattedImage, formattedMessage);
+    $("#header").prepend(formattedRole).prepend(formattedName).append(formattedBioPic, formattedMessage);
     $("#header").append(HTMLskillsStart);
 
 
-    for (var skill in bio.skills) {
-        var formattedSkills = HTMLskills.replace("%data%", bio.skills[skill]);
+    for (var i = 0; i < bio.skills.length; i++) {
+        var formattedSkills = HTMLskills.replace("%data%", bio.skills[i]);
         $("#skills").append(formattedSkills);
     }
 
@@ -89,45 +91,53 @@ bio.display = function() {
 };
 
 education.display = function() {
-    for (var school in education.schools) {
+    for (var i = 0; i < education.schools.length; i++) {
         $("#education").append(HTMLschoolStart);
 
-        var formattedName = HTMLschoolName.replace("%data%", education.schools[school].name);
-        var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
-        var formattedDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
-        var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
-        var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[school].majors);
+        var formattedName = HTMLschoolName.replace("%data%", education.schools[i].name);
+        var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[i].degree);
+        var formattedDates = HTMLschoolDates.replace("%data%", education.schools[i].dates);
+        var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[i].location);
+        var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[i].majors);
         $(".education-entry:last").append(formattedName + formattedDegree, formattedDates, formattedLocation, formattedMajor);
     }
 };
 
 work.display = function() {
-    for (var job in work.jobs) {
+    for (var i = 0; i < work.jobs.length; i++) {
         $("#workExperience").append(HTMLworkStart);
 
-        var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-        var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
-        var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
-        var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+        var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[i].employer);
+        var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[i].title);
+        var formattedDates = HTMLworkDates.replace("%data%", work.jobs[i].dates);
+        var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[i].description);
 
         $(".work-entry:last").append(formattedEmployer + formattedTitle, formattedDates, formattedDescription);
     }
 };
 
 projects.display = function() {
-    for (var item in projects.project) {
+    for (var i = 0; i < projects.project.length; i++){
         $("#projects").append(HTMLprojectStart);
-        var formattedTitle = HTMLprojectTitle.replace("%data%", projects.project[item].title);
-        var formattedDates = HTMLprojectDates.replace("%data%", projects.project[item].dates);
-        var formattedDescription = HTMLprojectDescription.replace("%data%", projects.project[item].description);
+        var formattedTitle = HTMLprojectTitle.replace("%data%", projects.project[i].title);
+        var formattedDates = HTMLprojectDates.replace("%data%", projects.project[i].dates);
+        var formattedDescription = HTMLprojectDescription.replace("%data%", projects.project[i].description);
 
         $(".project-entry:last").append(formattedTitle, formattedDates, formattedDescription);
-        for (var image in projects.project[item].images) {
-            var formattedImage = HTMLprojectImage.replace("%data%", projects.project[item].images[image]);
+        for (var j = 0; j < projects.project[i].images.length; j++) {
+            var formattedImage = HTMLprojectImage.replace("%data%", projects.project[i].images[j]);
             $(".project-entry:last").append(formattedImage);
         }
     }
 };
+
+
+
+bio.display();
+education.display();
+work.display();
+projects.display();
+
 
 function inName(name) {
     console.log(name);
@@ -136,12 +146,6 @@ function inName(name) {
 
     return newName;
 }
-
-
-work.display();
-projects.display();
-education.display();
-bio.display();
 
 $("#main").append(internationalizeButton);
 $("#mapDiv").append(googleMap);
